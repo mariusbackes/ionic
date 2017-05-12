e12abee34f239039995ca8d7b822a2e30c3e75f2
Marius Backes <mariusbackes@icloud.com>
2017-05-12 12:22:46 +0200

    Basic App With NavController function

---
+++ ionic2-basics/src/pages/users/users.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the UsersPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class UsersPage {
+
+
+}
